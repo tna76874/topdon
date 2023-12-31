5bae60e7639d8d084c5f32589075cb016fd60fb5
lmh <dev@hilberg.eu>
2023-12-31 15:09:06 +0100

    cleanup video class

--- topdon/video.py
+++ topdon/video.py
@@ -21,12 +21,22 @@ topdon_resolution = (256, 384)
 topdon_fps = 25.0
 topdon_usb_id = (3034, 22576)  # VID, PID
 
+
+
 log = logging.getLogger(__name__)
 
 class Video:
-    # queue 0 is for GUI, 1 is for recorder
-    frame_queue = [queue.Queue(1) for _ in range(2)]
-    video_running = False
+    known_cameras =     [
+                            {
+                                'name' : 'TC001',
+                                'device_id': ('0bda', '5830'),  # ('ID_VENDOR_ID', 'ID_MODEL_ID')
+                                'capabilities': 'capture',
+                                'in': [('capture','ID_V4L_CAPABILITIES')],
+                                'resolution': (256, 384),
+                                'fps': 25.0,
+                            },
+                        ]
+    model = None
 
     @staticmethod
     def list_cap_ids():
@@ -60,31 +70,46 @@ class Video:
 
             dev_port += 1
         return working_ids, available_ids, non_working_ids
+    
+    def list_devs(self):
+        context = pyudev.Context()
+        devices = [device for device in context.list_devices(subsystem='video4linux')]
+        for device in devices:
+            print(f"Device: {device}")
+            for key, value in device.items():
+                print(f"  {key}: {value}")
+            print("\n")
 
     # Sadly, Windows APIs / OpenCV is very limited, and the only way to detect the camera is by its characteristic resolution and framerate
     # On Linux, just use the VID/PID via udev
-    def get_topdon_cap_id(self):
+    def get_camera_cap_id(self):
         if platform.system() == 'Linux':
             for device in pyudev.Context().list_devices(subsystem='video4linux'):
-                try:
-                    if (int(device.get('ID_VENDOR_ID'), 16), int(device.get('ID_MODEL_ID'), 16)) == topdon_usb_id and \
-                            'capture' in device.get('ID_V4L_CAPABILITIES'):
-                        return device.get('DEVNAME')
-                except:
-                    pass
-            return None
+                for camera in self.known_cameras:
+                    try:
+                        if (device.get('ID_VENDOR_ID'), device.get('ID_MODEL_ID')) == camera['device_id'] and \
+                                all([k[0] in device.get(k[1]) for k in camera['in']]):
+                            self.camera = camera
+                            self.camera['DEVNAME'] = device.get('DEVNAME')
+                            return device.get('DEVNAME')
+                    except:
+                        pass
 
         # Fallback that uses the resolution and framerate to identify the device
         working_ids, _, _ = self.list_cap_ids()
-        for id in working_ids:
-            if id[1] == topdon_resolution and id[2] == topdon_fps:
-                return id[0]
+        for camera in self.known_cameras: 
+            for id in working_ids:
+                if id[1] == camera['resolution'] and id[2] == camera['fps']:
+                    self.camera = camera
+                    self.camera['DEVNAME'] = id[0]
+                    return id[0]
+
         return None
 
     def open(self, camera_id: Union[int, str] = -1):
         if camera_id == -1:
             log.info("No camera ID specified, scanning... (This could take a few seconds)")
-            camera_id = self.get_topdon_cap_id()
+            camera_id = self.get_camera_cap_id()
             if camera_id == None:
                 raise ConnectionError(f"Could not find camera module")
 
@@ -93,7 +118,6 @@ class Video:
         
         if (not cap.isOpened()):
             cap.release()
-            #raise ConnectionError(f"Could not open video capture device with index {camera_id}, is the module connected?")
 
         # check if resolution and FPS matches that of the P2 Pro module
         cap_res = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
@@ -107,67 +131,7 @@ class Video:
         
         self.cap = cap
 
-        # frame_counter = 0
 
-        # while True:
-        #     success, frame = cap.read()
-
-        #     if (not success):
-        #         continue
-
-        #     self.video_running = True
-            
-        #     # On Windows, with RGB conversion turned off, OpenCV returns the image as a 2D array with size [1][<imageLen>]. Turn into 1D array. 
-        #     if platform.system() == 'Windows':
-        #         frame = frame[0]
-
-        #     # split video frame (top is pseudo color, bottom is temperature data)
-        #     frame_mid_pos = int(len(frame) / 2)
-        #     picture_data = frame[0:frame_mid_pos]
-        #     thermal_data = frame[frame_mid_pos:]
-
-        #     # convert buffers to numpy arrays
-        #     yuv_picture = np.frombuffer(picture_data, dtype=np.uint8).reshape((topdon_resolution[1] // 2, topdon_resolution[0], 2))
-        #     rgb_picture = cv2.cvtColor(yuv_picture, cv2.COLOR_YUV2RGB_YUY2)
-        #     thermal_picture_16 = np.frombuffer(thermal_data, dtype=np.uint16).reshape((topdon_resolution[1] // 2, topdon_resolution[0]))
-
-        #     # pack parsed frame data into object
-        #     frame_obj = {
-        #         "frame_num": frame_counter,
-        #         "rgb_data": rgb_picture,
-        #         "yuv_data": yuv_picture,
-        #         "thermal_data": thermal_picture_16
-        #     }
-
-        #     # populate all queues with new frame
-        #     for queue in self.frame_queue:
-        #         # if queue is full, discard oldest frame (e.g. if frames not read fast enough or at all)
-        #         if queue.full():
-        #             queue.get(False)
-        #         queue.put(frame_obj)
-
-        #     frame_counter += 1
-
-
-if __name__ == "__main__":
-    # test stuff
-
-    # start = time.time()
-    # print("P2 Pro capture ID:", get_topdon_cap_id())
-    # print(time.time() - start)
-    # logging.basicConfig()
-    # log.setLevel(logging.INFO)
-    # Video().open()
-
-    # context = pyudev.Context()
-
-    
-    # devices = [device for device in context.list_devices(subsystem='video4linux')]
-
-    # for device in devices:
-    #     print(f"Device: {device}")
-    #     for key, value in device.items():
-    #         print(f"  {key}: {value}")
-    #     print("\n")
-        
-    pass
+if __name__ == "__main__":        
+    self = Video()
+    #self.list_devs()
